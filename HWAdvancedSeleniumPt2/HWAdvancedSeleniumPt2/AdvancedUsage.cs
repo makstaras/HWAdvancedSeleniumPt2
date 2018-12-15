6829d5338e519d51f87c056b9cd462a4bbd1e4c3
makstaras <makstaras@ukr.net>
2018-12-15 16:15:54 +0200

    Add the project

---
+++ HWAdvancedSeleniumPt2/HWAdvancedSeleniumPt2/AdvancedUsage.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using OpenQA.Selenium.Interactions;
+using System.Threading;
+
+namespace HWAdvancedSeleniumPt2
+{
+    public class AdvancedUsage
+    {
+        [TestFixture]
+        class BrowserOptions
+        {
+            IWebDriver driver;
+            IJavaScriptExecutor jexec;
+          
+            [OneTimeSetUp]
+            public void OneTimeSetUp()
+            {
+                var options = new ChromeOptions();
+                options.AddArguments
+                    (
+                        "--start-fullscreen",
+                        "--start-maximized",
+                        "--disable-infobars"
+                    );
+                
+                options.AddUserProfilePreference("download.default_directory", @"C:\test");
+
+                driver = new ChromeDriver(options);
+                driver.Url = "https://unsplash.com/search/photos/test";
+
+                jexec = (IJavaScriptExecutor)driver;
+            }
+
+            [Test]
+            public void DownloadLastItem()
+            {
+                while (true)
+                {
+                    var prevScrollY = Convert.ToInt64(jexec.ExecuteScript("return window.scrollY"));
+
+                    jexec.ExecuteScript("window.scrollBy(0, 400)");
+                    Thread.Sleep(500);
+
+                    var ScrollY = Convert.ToInt64(jexec.ExecuteScript("return window.scrollY"));
+
+                    if (prevScrollY == ScrollY)
+                        break;
+                }
+
+                var lastImage = driver.FindElements(By.XPath("//*[@id='gridMulti']/div[1]//a/div/img")).Last();
+                new Actions(driver).MoveToElement(lastImage).Perform();
+                driver.FindElements(By.CssSelector("#gridMulti > div:nth-child(1) a > span > svg > path")).Last().Click();
+
+                Assert.That(driver.FindElement(By.ClassName("_3Gbbu")).Text, Is.EqualTo("Say thanks"));              
+            }
+            
+            [OneTimeTearDown]
+            public void OneTimeTearDown()
+            {
+                driver.Quit();
+            }
+        }
+        }
+}
